f54c02c6363d2c3a2c6bb0188d9edf9e86f65291
Adedamola <adeniran.adebusola@gmail.com>
2019-08-05 21:25:05 +0100

    first commit

---
+++ app/src/main/java/com/xpacer/travelmantics/InsertActivity.java
@@ -0,0 +1,14 @@
+package com.xpacer.travelmantics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+
+}
